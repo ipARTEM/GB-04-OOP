e5a623e78ddb06f27bbccc2a21a79bf4d7d76ef6
Artem <a.khimin@yandex.ru>
2021-10-07 22:49:45 +0300

    Сделал 2.1

--- GB-04-02BankAccount/Program.cs
+++ GB-04-02BankAccount/Program.cs
@@ -6,7 +6,17 @@ namespace GB_04_02BankAccount
     {
         static void Main(string[] args)
         {
+            Random random =new Random();
+
+            Account account = new Account();
             
+            account.FillTypeAccount (TypeOfBankAccount.DEBIT);
+
+            account.FillNumber(random.Next(1, 99999999));
+
+            account.FillBalance(100000);
+
+            account.FullInfo();
         }
     }
 }
