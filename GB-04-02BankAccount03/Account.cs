d999e2c926a93e5fff6e4e0b5e14dc1db1c6be40
Artem <a.khimin@yandex.ru>
2021-10-07 23:37:17 +0300

    Сделал 2.3

---
+++ GB-04-02BankAccount03/Account.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_04_02BankAccount03
+{
+    public class Account
+    {
+        int _number;
+
+        static int counter = 1;
+
+        decimal _balance;
+
+        TypeOfBankAccount _typeAccount;
+
+        public Account()
+        {
+            _number = Increase();
+        }
+
+        public Account(decimal balance)
+        {
+            _balance = balance;
+            _number = Increase();
+
+        }
+        public Account(TypeOfBankAccount typeAccount)
+        {
+            _typeAccount = typeAccount;
+            _number = Increase();
+
+        }
+
+        public Account(decimal balance, TypeOfBankAccount typeAccount)
+        {
+            _balance = balance;
+            _typeAccount = typeAccount;
+            _number = Increase();
+
+        }
+
+        int Increase()
+        {
+            return counter++;
+        }
+
+        public int InfoNumber()
+        {
+            return _number;
+        }
+
+        public decimal InfoBalance()
+        {
+            return _balance;
+        }
+
+        public TypeOfBankAccount InfoTypeAccount()
+        {
+            return _typeAccount;
+        }
+
+        public void FillNumber(int number)
+        {
+            _number = number;
+        }
+
+        public void FillBalance(decimal balance)
+        {
+            _balance = balance;
+
+        }
+
+        public void FillTypeAccount(TypeOfBankAccount typeAccount)
+        {
+            _typeAccount = typeAccount;
+        }
+
+        public void FullInfo()
+        {
+            Console.WriteLine($"Тип счёта: {_typeAccount}, Номер счёта: {_number}, Баланс: {_balance}");
+        }
+
+    }
+}
---
+++ GB-04-02BankAccount03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GB_04_02BankAccount03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account(10);
+            Account account2 = new Account(TypeOfBankAccount.DEBIT);
+            Account account3 = new Account(100, TypeOfBankAccount.DEPOSIT);
+
+            account1.FullInfo();
+            account2.FullInfo();
+            account3.FullInfo();
+        }
+    }
+}
