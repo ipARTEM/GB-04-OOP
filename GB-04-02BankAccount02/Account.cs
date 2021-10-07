4a02d266fbaf4be2b5a8d87534e6583e39141f58
Artem <a.khimin@yandex.ru>
2021-10-07 23:07:49 +0300

    Сделал 2.2

---
+++ GB-04-02BankAccount02/Account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_04_02BankAccount02
+{
+    public class Account
+    {
+        int _number;
+
+        static int counter=1;
+
+        decimal _balance;
+
+        TypeOfBankAccount _typeAccount;
+
+        public Account()
+        {
+            _number= Increase();
+        }
+
+        int Increase()
+        {
+            return counter++;
+        }
+
+        public int InfoNumber()
+        {
+            return _number;
+        }
+
+        public decimal InfoBalance()
+        {
+            return _balance;
+        }
+
+        public TypeOfBankAccount InfoTypeAccount()
+        {
+            return _typeAccount;
+        }
+
+        public void FillNumber(int number)
+        {
+            _number = number;
+        }
+
+        public void FillBalance(decimal balance)
+        {
+            _balance = balance;
+
+        }
+
+        public void FillTypeAccount(TypeOfBankAccount typeAccount)
+        {
+            _typeAccount = typeAccount;
+        }
+
+        public void FullInfo()
+        {
+            Console.WriteLine($"Тип счёта: {_typeAccount}, Номер счёта: {_number}, Баланс: {_balance}");
+        }
+
+    }
+}
---
+++ GB-04-02BankAccount02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GB_04_02BankAccount02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Account account1 = new Account();
+            Account account2 = new Account();
+            Account account3 = new Account();
+
+            account1.FillTypeAccount(TypeOfBankAccount.DEBIT);
+
+            account1.FillBalance(100000);
+
+            account1.FullInfo();
+            account2.FullInfo();
+            account3.FullInfo();
+
+
+        }
+    }
+}
