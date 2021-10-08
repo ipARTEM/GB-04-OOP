d5ff31cff6ba3ffeddcdc2571b3688f0c4aca056
Artem <a.khimin@yandex.ru>
2021-10-08 09:42:26 +0300

    Сделал свойства

---
+++ GB-04-02BankAccount04/Account.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_04_02BankAccount04
+{
+    public class Account
+    {
+        static int counter = 1;
+
+        int _number;
+
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                if (value.GetType().ToString() == "System.Int32")
+                {
+                    _number = value;
+                }
+            }
+               
+        } 
+
+
+        decimal _balance;
+
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                if (value.GetType().ToString() == "System.Decimal")
+                {
+                    _balance = value;
+                }
+            }
+
+        }
+
+        TypeOfBankAccount _typeAccount;
+
+        public TypeOfBankAccount TypeAccount
+        {
+            get
+            {
+                return _typeAccount;
+            }
+            set
+            {
+                if (value.GetType().ToString() == "GB_04_02BankAccount04.TypeOfBankAccount")
+                {
+                    _typeAccount = value;
+                }
+            }
+
+        }
+
+        public Account()
+        {
+            _number = Increase();
+        }
+
+        public Account(decimal balance)
+        {
+            _balance = balance;
+            _number = Increase();
+
+        }
+        public Account(TypeOfBankAccount typeAccount)
+        {
+            _typeAccount = typeAccount;
+            _number = Increase();
+
+        }
+
+        public Account(decimal balance, TypeOfBankAccount typeAccount)
+        {
+            _balance = balance;
+            _typeAccount = typeAccount;
+            _number = Increase();
+
+        }
+
+        int Increase()
+        {
+            return counter++;
+        }
+
+        
+    }
+}
---
+++ GB-04-02BankAccount04/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GB_04_02BankAccount04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+     
+            
+        }
+    }
+}
