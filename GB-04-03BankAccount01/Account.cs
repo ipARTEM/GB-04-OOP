9a143872da69ee06c0d2c5cc0a4e7b5f565e4429
Artem <a.khimin@yandex.ru>
2021-10-15 19:06:30 +0300

    test

---
+++ GB-04-03BankAccount01/Account.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_04_03BankAccount01
+{
+    public class Account
+    {
+        static int counter = 1;
+
+        int _number;
+
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                if (value.GetType().ToString() == "System.Int32")
+                {
+                    _number = value;
+                }
+            }
+
+        }
+
+
+        decimal _balance;
+
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                if (value.GetType().ToString() == "System.Decimal")
+                {
+                    _balance = value;
+                }
+            }
+
+        }
+
+        TypeOfBankAccount _typeAccount;
+
+        public TypeOfBankAccount TypeAccount
+        {
+            get
+            {
+                return _typeAccount;
+            }
+            set
+            {
+                if (value.GetType().ToString() == "GB_04_02BankAccount04.TypeOfBankAccount")
+                {
+                    _typeAccount = value;
+                }
+            }
+
+        }
+
+        public Account()
+        {
+            _number = Increase();
+        }
+
+        public Account(decimal balance)
+        {
+            _balance = balance;
+            _number = Increase();
+
+        }
+        public Account(TypeOfBankAccount typeAccount)
+        {
+            _typeAccount = typeAccount;
+            _number = Increase();
+
+        }
+
+        public Account(decimal balance, TypeOfBankAccount typeAccount)
+        {
+            _balance = balance;
+            _typeAccount = typeAccount;
+            _number = Increase();
+
+        }
+
+        int Increase()
+        {
+            return counter++;
+        }
+
+        public void FullInfo()
+        {
+            Console.WriteLine($"Тип счёта: {_typeAccount}, Номер счёта: {_number}, Баланс: {_balance}");
+        }
+
+        /// <summary>
+        /// Положить на счет
+        /// </summary>
+        /// <param name="number">Номер счёта</param>
+        /// <param name="put">Положить сумму</param>
+        public void DepositMoney(int number, decimal put)
+        {
+            if (number == _number)
+            {
+                _balance = _balance + put;
+                Console.WriteLine($"Счёт пополнен на: {put}");
+
+            }
+
+        }
+
+        /// <summary>
+        /// Снять со счета
+        /// </summary>
+        /// <param name="number">Номер счёта</param>
+        /// <param name="take">Снять сумму</param>
+        public void WithdrawMoney(int number, decimal take)
+        {
+            if (number == _number)
+            {
+                if (_balance >= take)
+                {
+                    _balance = _balance - take;
+
+                    Console.WriteLine($"Со счёта снята на: {take}");
+                }
+                else
+                    Console.WriteLine($"На счету недостаточно средств. Баланс: {_balance}; Сумма для снятия: {take};");
+
+            }
+        }
+
+        /// <summary>
+        /// Перевод денег
+        /// </summary>
+        /// <param name="moneyFrom">Ссылка на объект класса банковский счет откуда снимаются деньги</param>
+        /// <param name="take">Сумма снятия</param>
+        public void MoneyTransfer(Account moneyFrom, decimal take)
+        {
+            if (moneyFrom._balance> take)
+            {
+                _balance =_balance + take;
+                moneyFrom._balance = moneyFrom._balance - take;
+                Console.WriteLine($"На счёт: {_number}; Зачислена сумма {take}; Остаток: {_balance};");
+                Console.WriteLine($"Со счёта: {moneyFrom._balance}; Списана сумма: {take}; Остаток: {_balance};");
+                
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ GB-04-03BankAccount01/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GB_04_03BankAccount01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> listAccount = new List<Account>();
+
+            Account account1 = new Account(100, TypeOfBankAccount.DEPOSIT);
+            Account account2 = new Account(99, TypeOfBankAccount.DEBIT);
+            Account account3 = new Account(1555, TypeOfBankAccount.CREDIT);
+            Account account4 = new Account(888, TypeOfBankAccount.BUDGET);
+
+            listAccount.Add(account1);
+            listAccount.Add(account2);
+            listAccount.Add(account3);
+            listAccount.Add(account4);
+
+            foreach (var item in listAccount)
+            {
+                item.FullInfo();
+            }
+
+            string exit;
+            do
+            {
+                Console.WriteLine("Для выхода нажмите Q + Enter. Для выбора текущего счёта нажмите номер счёта");
+
+                exit = Console.ReadLine();
+                exit= exit.ToLower();
+
+                
+                int _fromTheAccount=0;
+                bool _isAccountFound = false ;
+                decimal _take;
+
+
+
+                switch (exit)
+                {
+                    case "1":
+
+                        Console.WriteLine("Выбран текущий счёт № 1.");
+                        do
+                        {
+                            Console.WriteLine("Выберете счёт с которого будут переведены деньги:");
+
+                            try
+                            {
+                                _fromTheAccount = Convert.ToInt32(Console.ReadLine());
+                                foreach (var item in listAccount)
+                                {
+                                    if (item.Number == _fromTheAccount)
+                                    {
+                                             
+
+                                        _isAccountFound = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("Не корректный номер счёта");
+                            }
+
+                        } while (_isAccountFound);
+
+
+                        do
+                        {
+
+                            Console.WriteLine("Выберете сумму списания денег:");
+
+                            try
+                            {
+
+                            }
+                            catch (Exception)
+                            {
+
+                                throw;
+                            }
+
+                        } while (true);
+
+
+                        account1.MoneyTransfer(listAccount[_fromTheAccount-1], _take);
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+            } while (exit!="q");
+
+
+
+
+
+        }
+    }
+}
