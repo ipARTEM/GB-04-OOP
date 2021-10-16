c1f47d182bc59aea17001570916bdd2e695567ef
Artem <a.khimin@yandex.ru>
2021-10-16 15:33:30 +0300

    Сделал задание 3.2

---
+++ GB-04-03BankAccount02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GB_04_03BankAccount02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+
+            string str = Console.ReadLine();
+
+            str= Rev(str);
+
+            Console.WriteLine(str);
+
+
+        }
+
+        static string Rev(string str)
+        {
+
+            char[] reverse = str.ToCharArray();
+
+            Array.Reverse(reverse);
+
+            return new string(reverse);
+
+        }
+    }
+}
