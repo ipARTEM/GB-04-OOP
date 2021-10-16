68baa3ed981a0f28ef0fd0977cbd059d9cf42651
Artem <a.khimin@yandex.ru>
2021-10-16 16:59:49 +0300

    Сделал 3.3

---
+++ GB-04-03String03/Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_04_03String03
+{
+    public class Data
+    {
+      
+
+        
+
+    }
+}
