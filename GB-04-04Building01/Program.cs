ca01e40a02da03667cf6c39302b91b4707ea5013
Artem <a.khimin@yandex.ru>
2021-10-22 21:24:00 +0300

    Сделал задание 4.1

---
+++ GB-04-04Building01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GB_04_04Building01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Building building = new ();
+            building.Add(3,4,5);
+
+            Building building2 = new();
+            building2.Add(4, 8, 7, 5);
+
+            building.Print();
+            building2.Print();
+            building.Print();
+
+        }
+    }
+}
