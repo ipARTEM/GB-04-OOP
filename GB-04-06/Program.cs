c050808778961e63cdc7e3e379059351ec64554c
Artem <a.khimin@yandex.ru>
2021-11-18 17:33:01 +0300

    Подготовка под задание №6

---
+++ GB-04-06/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GB_04_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
